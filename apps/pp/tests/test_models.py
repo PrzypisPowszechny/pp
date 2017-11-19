c0a61702f00163e839256f4e33ff4336620009a2
kaglowka <krz.glowka@gmail.com>
2017-11-20 00:38:23 +0100

    Refactored test_models

--- apps/pp/tests/test_models.py
+++ apps/pp/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from apps.pp.models import Reference, User, UserReferenceFeedback, UserReferenceRequestFeedback, ReferenceRequest
+from apps.pp.models import Reference, UserReferenceFeedback, UserReferenceRequestFeedback, ReferenceRequest
 
 
 class UserModelTest(TestCase):
@@ -10,57 +10,41 @@ class UserModelTest(TestCase):
 
     def test_creating_multiple_users(self):
         for i in range(40):
-            user = get_user_model().objects.create_user(username="Rozbojnik_" + str(i + 1))
+            get_user_model().objects.create_user(username="Rozbojnik_" + str(i + 1))
         self.assertEqual(40, get_user_model().objects.count())
 
 
-class ReferenceModelTest(TestCase):
-    def setUp(self):
-        user = get_user_model().objects.create_user(username="Alibaba")
-        self.reference = Reference.objects.create(user=user, priority='NORMAL', comment="good job",
-                                                  link="www.przypispowszechny.com", link_title="very nice")
-
-    def test_creating_reference_without_user(self):
-        try:
-            reference = Reference.objects.create(priority='NORMAL', comment="good job",
-                                                 link="www.przypispowszechny.com", link_title="very nice")
-        except:
-            self.assertEqual(1, 1)
-        else:
-            self.assertEqual(0, 1)
-
-
 class UserReferenceFeedbackModelTest(TestCase):
-    def test_creating_userreferencefeedback(self):
-        user = get_user_model().objects.create_user(username="Alibaba")
-        reference = Reference.objects.create(user=user, priority='NORMAL', comment="good job",
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(username="Alibaba")
+        cls.user2 = get_user_model().objects.create_user(username="Rozbójnik")
+        cls.reference = Reference.objects.create(user=cls.user, priority='NORMAL', comment="good job",
                                              link="www.przypispowszechny.com", link_title="very nice")
-        urf = UserReferenceFeedback.objects.create(user=user, reference=reference, useful=True, objection=True)
+
+    def test_creating(self):
+        UserReferenceFeedback.objects.create(user=self.user, reference=self.reference, useful=True, objection=True)
         self.assertEqual(1, UserReferenceFeedback.objects.count())
 
-    def test_useful_userreferencefeedback(self):
-        user = get_user_model().objects.create_user(username="Alibaba")
-        reference = Reference.objects.create(user=user, priority='NORMAL', comment="good job",
-                                             link="www.przypispowszechny.com", link_title="very nice")
-        urf = UserReferenceFeedback.objects.create(user=user, reference=reference, useful=True, objection=True)
-        urf2 = UserReferenceFeedback.objects.get(user=user, reference=reference)
-        self.assertEqual(True, urf2.useful)
+    def test_useful(self):
+        UserReferenceFeedback.objects.create(user=self.user, reference=self.reference, useful=True, objection=True)
+        urf2 = UserReferenceFeedback.objects.get(user=self.user, reference=self.reference)
+        self.assertTrue(urf2.useful)
 
     def test_updating_reference_useful_and_objection_count(self):
-        user = get_user_model().objects.create_user(username="Alibaba")
-        user2 = get_user_model().objects.create_user(username="Rozbojnik")
-        reference = Reference.objects.create(user=user, priority='NORMAL', comment="good job",
-                                             link="www.przypispowszechny.com", link_title="very nice")
-        urf = UserReferenceFeedback.objects.create(user=user, reference=reference, useful=True, objection=True)
-        UserReferenceFeedback.objects.create(user=user2, reference=reference, useful=True, objection=False)
-        reference.count_useful_and_objection()
-        self.assertEqual(2, reference.useful_count)
-        self.assertEqual(1, reference.objection_count)
+        UserReferenceFeedback.objects.create(user=self.user, reference=self.reference, useful=False, objection=True)
+        UserReferenceFeedback.objects.create(user=self.user2, reference=self.reference, useful=True, objection=False)
+        self.reference.count_useful_and_objection()
+        self.assertEqual(1, self.reference.useful_count)
+        self.assertEqual(1, self.reference.objection_count)
 
 
 class UserReferenceRequestFeedbackModelTest(TestCase):
-    def test_creating_userreferencerequestfeedback(self):
-        user = get_user_model().objects.create_user(username="Alibaba")
-        rr = ReferenceRequest.objects.create(user=user)
-        urrf = UserReferenceRequestFeedback.objects.create(user=user, reference_request=rr, )
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username="Alibaba")
+
+    def test_creating(self):
+        rr = ReferenceRequest.objects.create(user=self.user)
+        UserReferenceRequestFeedback.objects.create(user=self.user, reference_request=rr, )
         self.assertEqual(1, UserReferenceRequestFeedback.objects.count())
