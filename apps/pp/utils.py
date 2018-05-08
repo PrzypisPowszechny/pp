d71d12fb9b02f0c47edb501584195fd6c49e8abc
Tomek Glowka <tk.glowka@gmail.com>
2018-05-08 15:37:48 +0200

    Add standardize_url utils

--- apps/pp/utils.py
+++ apps/pp/utils.py
@@ -1,7 +1,7 @@
 import collections
+from urllib.parse import urlencode, parse_qsl, urlsplit
 
 from django.db import models
-from rest_framework import serializers
 
 
 def get_relationship_id(root_serializer, name):
@@ -89,3 +89,53 @@ class DataPreSerializer(object):
                 'data': data,
                 'links': self.root_obj.id
             }
+
+
+OMITTED_QUERY_VARS = (
+    'utm_campaign',
+    'utm_medium',
+)
+
+
+def standardize_url_index(data):
+    """
+    Format url in the way that:
+      - ignores protocol
+      - ignores fragment(anchor)
+      - ignores some blacklisted query vars like utm etc
+      - set '/' as a path if none given
+      - removes '?' if no query string
+    """
+    url_parsed = urlsplit(data)
+    query_tuples = parse_qsl(url_parsed.query)
+    new_query_tuples = []
+    for var_name, val in query_tuples:
+        if var_name not in OMITTED_QUERY_VARS:
+            new_query_tuples.append((var_name, val))
+    return '{netloc}{path}{query}'.format(
+        netloc=url_parsed.netloc,
+        path=url_parsed.path if url_parsed.path else '/',
+        query='?' + urlencode(new_query_tuples) if new_query_tuples else ''
+    )
+
+
+def standardize_url(data):
+    """
+        Format url in the way that:
+          - ignores fragment(anchor)
+          - ignores some blacklisted query vars like utm etc
+          - set '/' as a path if none given
+          - removes '?' if no query string
+        """
+    url_parsed = urlsplit(data)
+    query_tuples = parse_qsl(url_parsed.query)
+    new_query_tuples = []
+    for var_name, val in query_tuples:
+        if var_name not in OMITTED_QUERY_VARS:
+            new_query_tuples.append((var_name, val))
+    return '{scheme}://{netloc}{path}{query}'.format(
+        scheme=url_parsed.scheme,
+        netloc=url_parsed.netloc,
+        path=url_parsed.path if url_parsed.path else '/',
+        query='?' + urlencode(new_query_tuples) if new_query_tuples else ''
+    )
