8f826ae35aaa3282f9b47dd45382893b30097751
Krzysztof <krz.glowka@gmail.com>
2018-12-02 00:59:49 +0100

    Whitelist (CORS) panel.devdeploy1.przypispowszechny.pl and turn on domain cookie sharing

--- settings/heroku_dev.py
+++ settings/heroku_dev.py
@@ -13,9 +13,11 @@ ALLOWED_HOSTS = [
 CORS_ORIGIN_WHITELIST = ()
 CORS_ORIGIN_REGEX_WHITELIST = (
     r'https?://localhost:.*',
-    r'https?://panel.przypispowszechny.pl(/.*)?'
+    r'https?://([0-9a-zA-Z_.-]+\.)?przypispowszechny\.pl(/.*)?'
 )
 
+# Allow to share cookies between the primary dev domain and its subdomains such as panel.devdeploy1.przypispowszechny.pl
+SESSION_COOKIE_DOMAIN = 'devdeploy1.przypispowszechny.pl'
 
 # Honor the X-Forwarded-Host header to know real domain (one from ALLOWED_HOSTS)
 USE_X_FORWARDED_HOST = True
