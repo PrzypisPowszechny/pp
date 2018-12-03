4abca353bb5040cb530c1419cec9527ec20a1849
Tomek Glowka <tk.glowka@gmail.com>
2018-12-03 19:11:19 +0100

    Improve logging of demagog deserialization warnings

--- apps/publisher/consumers.py
+++ apps/publisher/consumers.py
@@ -20,7 +20,8 @@ class DemagogConsumer(JSONConsumer):
         return super().get(endpoint_path, params)
 
     def get_all_statements(self, page=1):
-        response = self.get('/statements', params={
+        endpoint = '/statements'
+        response = self.get(endpoint, params={
             'page': page,
             'q': 'all',
             'client': 'pp'
@@ -31,32 +32,41 @@ class DemagogConsumer(JSONConsumer):
             )
         deserializer = StatementDeserializer(many=True, data=response.get('data'))
         if not deserializer.is_valid():
-            raise DemagogConsumer.ConsumingDataError(self.request_deserialize_error(deserializer.errors))
+            raise DemagogConsumer.ConsumingDataError(self.request_deserialize_error(
+                deserializer.errors, for_endpoint=endpoint,
+            ))
         return response['total_pages'], response['current_page'], deserializer.validated_data
 
+    # TODO: reshape into fully object-oriented programming model, with method acting as separate class
     def get_statements(self, source_url):
-        response = self.get('/statements', params={
+        endpoint = '/statements'
+        response = self.get(endpoint, params={
             'uri': source_url,
             'client': 'pp'
         })
         deserializer = StatementDeserializer(many=True, data=response.get('data'))
         if not deserializer.is_valid():
-            raise DemagogConsumer.ConsumingDataError(self.request_deserialize_error(deserializer.errors))
+            raise DemagogConsumer.ConsumingDataError(self.request_deserialize_error(
+                deserializer.errors, for_endpoint=endpoint, for_params={'uri': source_url})
+            )
         return deserializer.validated_data
 
     def get_sources_list(self):
-        response = self.get('/sources_list', params={
+        endpoint = '/sources_list'
+        response = self.get(endpoint, params={
             'client': 'pp'
         })
         deserializer = SourcesDeserializer(data=response.get('data'))
         if not deserializer.is_valid():
-            raise DemagogConsumer.ConsumingDataError(self.request_deserialize_error(deserializer.errors))
+            raise DemagogConsumer.ConsumingDataError(self.request_deserialize_error(
+                deserializer.errors, for_endpoint=endpoint,
+            ))
 
         valid_sources, invalid_sources = split_valid_urls(deserializer.validated_data['attributes']['sources'])
         if invalid_sources:
             logging.warning(self.request_deserialize_error(
                 '{invalid}/{all} of loaded sources are not valid urls: \n {sources}'.format(
                     invalid=len(invalid_sources), all=len(invalid_sources)+len(valid_sources), sources=invalid_sources
-                )
+                ), for_endpoint=endpoint
             ))
         return valid_sources
