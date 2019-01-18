a72bb11d6a9f51c017e75dd6afe3c8f09457c09e
Krzysztof <krz.glowka@gmail.com>
2019-01-18 21:08:41 +0100

    Update heroku_prod CORS settings to allow access for external dashboard app

--- settings/heroku_prod.py
+++ settings/heroku_prod.py
@@ -10,8 +10,19 @@ PP_MAIL_DOMAIN='mail.przypispowszechny.pl'
 MAILGUN_API_URL='https://api.mailgun.net/v3/{}/messages'.format(PP_MAIL_DOMAIN)
 
 ALLOWED_HOSTS = [
-    'przypispowszechny.pl', 'www.przypispowszechny.pl', 'app.przypispowszechny.pl',
+    'przypispowszechny.pl', 'www.przypispowszechny.pl',
+    'app.przypispowszechny.pl', 'www.app.przypispowszechny.pl',
 ]
+
+CORS_ORIGIN_WHITELIST = ()
+# Allow usage from applications hosted on subdomains
+CORS_ORIGIN_REGEX_WHITELIST = (
+    r'https?://([0-9a-zA-Z_.-]+\.)?przypispowszechny\.pl(/.*)?'
+)
+CORS_ALLOW_CREDENTIALS = True
+
+# Allow to share cookies between the primary dev domain and its subdomains such as panel.devdeploy1.przypispowszechny.pl
+SESSION_COOKIE_DOMAIN = 'devdeploy1.przypispowszechny.pl'
 
 # Honor the X-Forwarded-Host header to know real domain (one from ALLOWED_HOSTS)
 USE_X_FORWARDED_HOST = True
